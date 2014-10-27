ec241659b123c023641302b623b4f37ff03fe4d4
julienCXX <github@chmodplusx.eu>
2014-10-27 19:35:41 -0300

    removed unused imports in CardMenuItem

--- client/src/main/java/ar/edu/itba/pod/mmxivii/sube/client/items/generic/CardMenuItem.java
+++ client/src/main/java/ar/edu/itba/pod/mmxivii/sube/client/items/generic/CardMenuItem.java
@@ -1,20 +1,9 @@
 package ar.edu.itba.pod.mmxivii.sube.client.items.generic;
 
-import ar.edu.itba.pod.mmxivii.sube.client.exceptions.CannotProcessRequestException;
-import ar.edu.itba.pod.mmxivii.sube.client.exceptions.CardNotFoundException;
 import ar.edu.itba.pod.mmxivii.sube.client.exceptions.CardOperationException;
-import ar.edu.itba.pod.mmxivii.sube.client.exceptions.CommunicationsFailureException;
-import ar.edu.itba.pod.mmxivii.sube.client.exceptions.OperationNotPermittedByBalanceException;
 import ar.edu.itba.pod.mmxivii.sube.client.exceptions.OperationReturnConverter;
-import ar.edu.itba.pod.mmxivii.sube.client.exceptions.ServiceTimeoutException;
-import ar.edu.itba.pod.mmxivii.sube.client.exceptions.UnknownCardOperationException;
 import ar.edu.itba.pod.mmxivii.sube.common.Card;
 import ar.edu.itba.pod.mmxivii.sube.common.CardClient;
-import static ar.edu.itba.pod.mmxivii.sube.common.CardRegistry.CANNOT_PROCESS_REQUEST;
-import static ar.edu.itba.pod.mmxivii.sube.common.CardRegistry.CARD_NOT_FOUND;
-import static ar.edu.itba.pod.mmxivii.sube.common.CardRegistry.COMMUNICATIONS_FAILURE;
-import static ar.edu.itba.pod.mmxivii.sube.common.CardRegistry.OPERATION_NOT_PERMITTED_BY_BALANCE;
-import static ar.edu.itba.pod.mmxivii.sube.common.CardRegistry.SERVICE_TIMEOUT;
 
 /**
  * A menu item using the CardClient object and a choosen card.
