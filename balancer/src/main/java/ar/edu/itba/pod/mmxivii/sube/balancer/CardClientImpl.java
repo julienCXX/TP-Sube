923b98b4f4bdd0e336b87f7aac07123f6ec894ea
Teresa <teresa@collossus.com>
2014-10-27 17:39:58 -0300

    recover handling timeout fixed?

--- balancer/src/main/java/ar/edu/itba/pod/mmxivii/sube/balancer/CardClientImpl.java
+++ balancer/src/main/java/ar/edu/itba/pod/mmxivii/sube/balancer/CardClientImpl.java
@@ -4,12 +4,16 @@ import ar.edu.itba.pod.mmxivii.sube.common.*;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+
 import java.rmi.ConnectException;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.server.UID;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 import static ar.edu.itba.pod.mmxivii.sube.common.Utils.CARD_REGISTRY_BIND;
 import static ar.edu.itba.pod.mmxivii.sube.common.Utils.checkNotNull;
@@ -19,6 +23,7 @@ public class CardClientImpl extends UnicastRemoteObject implements CardClient {
     private static final long serialVersionUID = 3498345765116694167L;
     private CardRegistry cardRegistry;
     private final CardServiceRegistryImpl cardServiceRegistry;
+    private static final long TIME_OUT = 50; // en milisegundos
 
     public CardClientImpl(@Nonnull CardRegistry cardRegistry,
                           @Nonnull CardServiceRegistryImpl cardServiceRegistry)
@@ -74,52 +79,102 @@ public class CardClientImpl extends UnicastRemoteObject implements CardClient {
     @Override
     public double getCardBalance(@Nonnull UID id) throws RemoteException {
 
-        CardService s = getCardService();
-        assignOperation(s);
-        double result;
-        try {
-            result = s.getCardBalance(id);
-        } catch (ConnectException e) {
-            cardServiceRegistry.troubleshootFailedService(s);
-            return CardRegistry.COMMUNICATIONS_FAILURE;
-        }
-        leaveOperation(s);
-        return result;
+//        CardService s = getCardService();
+//        assignOperation(s);
+//        double result;
+//        try {
+//            result = s.getCardBalance(id);
+//        } catch (ConnectException e) {
+//            cardServiceRegistry.troubleshootFailedService(s);
+//            return CardRegistry.COMMUNICATIONS_FAILURE;
+//        }
+//        leaveOperation(s);
+//        return result;
+        
+        ExecutorService es = Executors.newCachedThreadPool();
+		TimeoutTravelChecker TimeoutCheck = new TimeoutTravelChecker(
+				cardServiceRegistry, this, Action.GETCARDBALANCE, id, "", 0);
+
+		es.execute(TimeoutCheck);
+		try {
+			if (es.awaitTermination(CardClientImpl.TIME_OUT,
+					TimeUnit.MILLISECONDS)) {
+				return CardRegistry.SERVICE_TIMEOUT;
+			} else {
+				return TimeoutCheck.getResult();
+			}
+		} catch (InterruptedException e) {
+			return CardRegistry.CANNOT_PROCESS_REQUEST;
+		}
+
 
     }
 
     @Override
     public double travel(@Nonnull UID id, @Nonnull String description,
                          double amount) throws RemoteException {
-        CardService s = getCardService();
-        assignOperation(s);
-        double result;
-        try {
-            result = s.travel(id, description, amount);
-        } catch (ConnectException e) {
-            cardServiceRegistry.troubleshootFailedService(s);
-            return CardRegistry.COMMUNICATIONS_FAILURE;
-        }
-        leaveOperation(s);
-        return result;
+//        CardService s = getCardService();
+//        assignOperation(s);
+//        double result;
+//        try {
+//            result = s.travel(id, description, amount);
+//        } catch (ConnectException e) {
+//            cardServiceRegistry.troubleshootFailedService(s);
+//            return CardRegistry.COMMUNICATIONS_FAILURE;
+//        }
+//        leaveOperation(s);
+//        return result;
+    	ExecutorService es = Executors.newCachedThreadPool();
+		TimeoutTravelChecker TimeoutCheck = new TimeoutTravelChecker(
+				cardServiceRegistry, this, Action.TRAVEL, id, description, amount);
+		es.execute(TimeoutCheck);
 
+		try {
+			if (es.awaitTermination(CardClientImpl.TIME_OUT,
+					TimeUnit.MILLISECONDS)) {
+				return CardRegistry.SERVICE_TIMEOUT;
+			} else {
+				return TimeoutCheck.getResult();
+			}
+		} catch (InterruptedException e) {
+			return CardRegistry.CANNOT_PROCESS_REQUEST;
+		}
+
+    	
     }
 
     @Override
     public double recharge(@Nonnull UID id, @Nonnull String description,
                            double amount) throws RemoteException {
-        CardService s = getCardService();
-        assignOperation(s);
-        // @ToDo catch de excepciones
-        double result;
-        try {
-            result = s.recharge(id, description, amount);
-        } catch (ConnectException e) {
-            cardServiceRegistry.troubleshootFailedService(s);
-            return CardRegistry.COMMUNICATIONS_FAILURE;
-        }
-        leaveOperation(s);
-        return result;
+//        CardService s = getCardService();
+//        assignOperation(s);
+//        // @ToDo catch de excepciones
+//        double result;
+//        try {
+//            result = s.recharge(id, description, amount);
+//        } catch (ConnectException e) {
+//            cardServiceRegistry.troubleshootFailedService(s);
+//            return CardRegistry.COMMUNICATIONS_FAILURE;
+//        }
+//        leaveOperation(s);
+//        return result;
+
+    	ExecutorService es = Executors.newCachedThreadPool();
+		TimeoutTravelChecker TimeoutCheck = new TimeoutTravelChecker(
+				cardServiceRegistry, this, Action.RECHARGE, id, description, amount);
+		es.execute(TimeoutCheck);
+
+		try {
+			if (es.awaitTermination(CardClientImpl.TIME_OUT,
+					TimeUnit.MILLISECONDS)) {
+				return CardRegistry.SERVICE_TIMEOUT;
+			} else {
+				return TimeoutCheck.getResult();
+			}
+		} catch (InterruptedException e) {
+			return CardRegistry.CANNOT_PROCESS_REQUEST;
+		}
+
     }
 
     private CardService getCardService() {
@@ -138,5 +193,76 @@ public class CardClientImpl extends UnicastRemoteObject implements CardClient {
     public ConcurrentHashMap<UID, Double> synchronizeToServer() throws RemoteException {
         return null;
     }
+    
+    	private static class TimeoutTravelChecker implements Runnable {
+    	
+    			private CardServiceRegistryImpl cardRegistry;
+    			private CardClientImpl c;
+    			private Action action;
+    			private UID id;
+    			private String description;
+    			private double amount;
+    			private double result = 0;
+    	
+    			TimeoutTravelChecker(CardServiceRegistryImpl cardRegistry,
+    					CardClientImpl c, Action action, @Nonnull UID id,
+    				@Nonnull String description, double amount) {
+    				this.cardRegistry = cardRegistry;
+    				this.c = c;
+    				this.action = action;
+    				this.id = id;
+    				this.description = description;
+    				this.amount = amount;
+    			}
+    	
+    			@Override
+    			public void run() {
+    				CardService s = cardRegistry.getCardService();
+    			if (action.equals(Action.GETCARDBALANCE)) {
+    					c.assignOperation(s);
+    	
+    					try {
+    						result = s.getCardBalance(id);
+    					} catch (RemoteException e) {
+    						cardRegistry.troubleshootFailedService(s);
+    						result = CardRegistry.COMMUNICATIONS_FAILURE;
+    					}
+    	
+    					c.leaveOperation(s);
+    				} else if (action.equals(Action.TRAVEL)) {
+    					c.assignOperation(s);
+    	
+    					try {
+    						result = s.travel(id, description, amount);
+    				} catch (RemoteException e) {
+    						cardRegistry.troubleshootFailedService(s);
+    					result = CardRegistry.COMMUNICATIONS_FAILURE;
+    					}
+    	
+    					c.leaveOperation(s);
+    				} else if (action.equals(Action.RECHARGE)) {
+    					c.assignOperation(s);
+    	
+    					try {
+    						result = s.recharge(id, description, amount);
+    					} catch (RemoteException e) {
+    						cardRegistry.troubleshootFailedService(s);
+    						result = CardRegistry.COMMUNICATIONS_FAILURE;
+    					}
+    	
+    					c.leaveOperation(s);
+    				}
+    	
+    			}
+    	
+    			public double getResult() {
+    				return this.result;
+    			}
+    	
+    		}
+    	
+    		private enum Action {
+    			TRAVEL, RECHARGE, GETCARDBALANCE;
+    		}
 
 }
